a998cdf025c34ea232c1281e6314df609a5fa1fc
Rich Posert <rich@posert.com>
2020-08-28 16:13:14 -0700

    Remove plot tilt shift

--- orientation-plotter.R
+++ orientation-plotter.R
@@ -6,7 +6,9 @@ args = commandArgs(trailingOnly = TRUE)
 setwd(args[1])
 
 data <- read_csv(args[2], col_types = 'ddd') %>%
-  mutate(Tilt = Tilt/180 - 0.5, Psi = Psi/180, Rot = Rot/180)
+  mutate(Tilt = Tilt/180, Psi = Psi/180, Rot = Rot/180)
+
+# rot is phi, tilt is theta
 
 psi_rot = if_else(args[3] == 'True', 'Psi', 'Rot')
 
@@ -15,8 +17,8 @@ data %>%
   theme_minimal() +
   geom_hex(binwidth = 0.04) +
   scale_fill_viridis_c(name = 'Images') +
-  expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
-  scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  expand_limits(x = c(-1, 1), y = c(0,1)) +
+  scale_y_continuous(breaks = c(seq(0,1, by = 0.25))) +
   xlab(psi_rot) +
   coord_fixed()
 ggsave('linear_heatmap.png', width = 5, height = 3)
@@ -30,8 +32,8 @@ data %>%
                        trans = 'log',
                        name = 'Images',
                        breaks = breaks_log()) +
-  expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
-  scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  expand_limits(x = c(-1, 1), y = c(0,1)) +
+  scale_y_continuous(breaks = c(seq(0,1, by = 0.25))) +
   xlab(psi_rot) +
   coord_fixed() +
   ggtitle('This emulates Jet. Don\'t use!')
@@ -43,8 +45,8 @@ data %>%
   geom_hex(binwidth = 0.04) +
   scale_fill_viridis_c(trans = 'log', name = 'Images',
                        breaks = breaks_log()) +
-  expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
-  scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  expand_limits(x = c(-1, 1), y = c(0,1)) +
+  scale_y_continuous(breaks = c(seq(0,1, by = 0.25))) +
   xlab(psi_rot) +
   coord_fixed()
 ggsave('log_heatmap.png', width = 5, height = 3)
@@ -55,8 +57,8 @@ data %>%
   geom_bin2d(binwidth = 0.04) +
   scale_fill_viridis_c(trans = 'log', name = 'Images',
                        breaks = breaks_log()) +
-  expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
-  scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  expand_limits(x = c(-1, 1), y = c(0,1)) +
+  scale_y_continuous(breaks = c(seq(0,1, by = 0.25))) +
   scale_x_continuous(breaks = c(-0.5, 0, 0.5, 1.0)) +
   xlab(psi_rot) +
   coord_polar()
