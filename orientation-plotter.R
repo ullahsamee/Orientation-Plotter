2f644581c63a66bfba86fda261363a959e8dd4cc
Rich Posert <rich@posert.com>
2020-08-28 11:27:02 -0700

    Label log plots

--- orientation-plotter.R
+++ orientation-plotter.R
@@ -1,24 +1,44 @@
-library(tidyverse)
-library(ggplot2)
+suppressMessages(library(tidyverse))
+suppressMessages(library(ggplot2))
+suppressMessages(library(scales))
 
-data <- read_csv('out.csv', col_types = 'dd') %>% 
+args = commandArgs(trailingOnly = TRUE)
+setwd(args[1])
+
+data <- read_csv(args[2], col_types = 'dd') %>%
   mutate(Tilt = Tilt/180 - 0.5, Psi = Psi/180)
 
-data %>% 
+data %>%
   ggplot(aes(x = Psi, y = Tilt)) +
   theme_minimal() +
   geom_hex(binwidth = 0.04) +
-  scale_fill_viridis_c() +
+  scale_fill_viridis_c(name = 'Number of images') +
   expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
   scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
   coord_fixed()
 ggsave('linear_heatmap.png', width = 5, height = 3)
 
-data %>% 
+data %>%
   ggplot(aes(x = Psi, y = Tilt)) +
   theme_minimal() +
   geom_hex(binwidth = 0.04) +
-  scale_fill_viridis_c(trans = 'log') +
+  scale_fill_gradientn(colors = c("#00007F", "blue", "#007FFF", "cyan",
+                                  "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"),
+                       trans = 'log',
+                       name = 'log(images)',
+                       breaks = breaks_log()) +
+  expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
+  scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  coord_fixed() +
+  ggtitle('This emulates Jet. Don\'t use!')
+ggsave('jet_heatmap.png', width = 5, height = 3)
+
+data %>%
+  ggplot(aes(x = Psi, y = Tilt)) +
+  theme_minimal() +
+  geom_hex(binwidth = 0.04) +
+  scale_fill_viridis_c(trans = 'log', name = 'log(images)',
+                       breaks = breaks_log()) +
   expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
   scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
   coord_fixed()
--- orientation-plotter.py
+++ orientation-plotter.py
@@ -1,14 +1,17 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-import sys
 import pystar
 import csv
 import subprocess
+import argparse
+import os
 
 
 def main():
-    print(f'Reading {sys.argv[1]}')
-    document = pystar.load(sys.argv[1])
+    args = parser.parse_args()
+    script_location = os.path.dirname(os.path.realpath(__file__))
+
+    document = pystar.load(args.star_file)
 
     inner_dict = list(document['images'].items())[0]
     headers = inner_dict[0]
@@ -19,15 +22,24 @@ def main():
 
     angles = [(x[index_angle_tilt], x[index_angle_psi]) for x in particles]
 
-    print('Writing angles')
-    with open('out.csv', 'w', newline = '') as out:
-        csv_out = csv.writer(out)
-        csv_out.writerow(('Tilt', 'Psi'))
+    with open(args.csv_out, 'w', newline = '') as out:
+        outfile = csv.writer(out)
+        outfile.writerow(('Tilt', 'Psi'))
         for row in angles:
-            csv_out.writerow(row)
+            outfile.writerow(row)
 
-    print('Making plots')
-    subprocess.run(['Rscript', 'orientation-plotter.R'])
+    if not args.no_plots:
+        split_path = os.path.split(args.csv_out)
+        subprocess.run(['Rscript', os.path.join(script_location, 'orientation-plotter.R'),
+                        split_path[0], split_path[1]])
+
+
+parser = argparse.ArgumentParser(description = 'Re-plot particle orientations using viridis color scheme. Death to Jet!')
+parser.add_argument('star_file', help = '.star file with particle orientations')
+parser.add_argument('-o', '--csv-out', help = 'Name of output csv. Default out.csv',
+                    default = 'out.csv', type = str)
+parser.add_argument('--no-plots', help = 'Do not plot orientations. CSV only.',
+                    action = 'store_true')
 
 
 if __name__ == '__main__':
