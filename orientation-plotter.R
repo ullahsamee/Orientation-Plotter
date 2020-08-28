4faf7dcba76aa61f46c5c5304769c431cd382551
Rich Posert <rich@posert.com>
2020-08-28 12:52:45 -0700

    Use Rot by default

--- orientation-plotter.R
+++ orientation-plotter.R
@@ -5,41 +5,46 @@ suppressMessages(library(scales))
 args = commandArgs(trailingOnly = TRUE)
 setwd(args[1])
 
-data <- read_csv(args[2], col_types = 'dd') %>%
-  mutate(Tilt = Tilt/180 - 0.5, Psi = Psi/180)
+data <- read_csv(args[2], col_types = 'ddd') %>%
+  mutate(Tilt = Tilt/180 - 0.5, Psi = Psi/180, Rot = Rot/180)
+
+psi_rot = if_else(args[3] == 'True', 'Psi', 'Rot')
 
 data %>%
-  ggplot(aes(x = Psi, y = Tilt)) +
+  ggplot(aes(x = get(psi_rot), y = Tilt)) +
   theme_minimal() +
   geom_hex(binwidth = 0.04) +
-  scale_fill_viridis_c(name = 'Number of images') +
+  scale_fill_viridis_c(name = 'Images') +
   expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
   scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  xlab(psi_rot) +
   coord_fixed()
 ggsave('linear_heatmap.png', width = 5, height = 3)
 
 data %>%
-  ggplot(aes(x = Psi, y = Tilt)) +
+  ggplot(aes(x = get(psi_rot), y = Tilt)) +
   theme_minimal() +
   geom_hex(binwidth = 0.04) +
   scale_fill_gradientn(colors = c("#00007F", "blue", "#007FFF", "cyan",
                                   "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"),
                        trans = 'log',
-                       name = 'log(images)',
+                       name = 'Images',
                        breaks = breaks_log()) +
   expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
   scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  xlab(psi_rot) +
   coord_fixed() +
   ggtitle('This emulates Jet. Don\'t use!')
 ggsave('jet_heatmap.png', width = 5, height = 3)
 
 data %>%
-  ggplot(aes(x = Psi, y = Tilt)) +
+  ggplot(aes(x = get(psi_rot), y = Tilt)) +
   theme_minimal() +
   geom_hex(binwidth = 0.04) +
-  scale_fill_viridis_c(trans = 'log', name = 'log(images)',
+  scale_fill_viridis_c(trans = 'log', name = 'Images',
                        breaks = breaks_log()) +
   expand_limits(x = c(-1, 1), y = c(-0.5, 0.5)) +
   scale_y_continuous(breaks = c(-0.5, -0.25, 0, 0.25, 0.5)) +
+  xlab(psi_rot) +
   coord_fixed()
 ggsave('log_heatmap.png', width = 5, height = 3)
--- orientation-plotter.py
+++ orientation-plotter.py
@@ -10,6 +10,10 @@ import os
 def main():
     args = parser.parse_args()
     script_location = os.path.dirname(os.path.realpath(__file__))
+    if args.psi:
+        psi_rot = True
+    else:
+        psi_rot = False
 
     document = pystar.load(args.star_file)
 
@@ -19,26 +23,29 @@ def main():
 
     index_angle_tilt = headers.index('rlnAngleTilt')
     index_angle_psi = headers.index('rlnAnglePsi')
+    index_angle_rot = headers.index('rlnAngleRot')
 
-    angles = [(x[index_angle_tilt], x[index_angle_psi]) for x in particles]
+    angles = [(x[index_angle_tilt], x[index_angle_psi], x[index_angle_rot]) for x in particles]
 
     with open(args.csv_out, 'w', newline = '') as out:
         outfile = csv.writer(out)
-        outfile.writerow(('Tilt', 'Psi'))
+        outfile.writerow(('Tilt', 'Psi', 'Rot'))
         for row in angles:
             outfile.writerow(row)
 
     if not args.no_plots:
         split_path = os.path.split(args.csv_out)
         subprocess.run(['Rscript', os.path.join(script_location, 'orientation-plotter.R'),
-                        split_path[0], split_path[1]])
+                        split_path[0], split_path[1], str(psi_rot)])
 
 
 parser = argparse.ArgumentParser(description = 'Re-plot particle orientations using viridis color scheme. Death to Jet!')
 parser.add_argument('star_file', help = '.star file with particle orientations')
 parser.add_argument('-o', '--csv-out', help = 'Name of output csv. Default out.csv',
-                    default = 'out.csv', type = str)
+                    default = os.path.join('.', 'out.csv'), type = str)
 parser.add_argument('--no-plots', help = 'Do not plot orientations. CSV only.',
+                    action = 'store_true', default = False)
+parser.add_argument('--psi', help = 'Plot tilt and psi instead of tilt and rot',
                     action = 'store_true')
 
 
